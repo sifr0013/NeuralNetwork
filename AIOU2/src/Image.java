00f66ea2c406441c2895ed5df2e37ca9ad850a0c
sifr0013 <sifr0013@student.umu.se>
2017-10-03 20:55:23 +0200

    Implemented some of the Image.java

--- AIOU2/src/Image.java
+++ AIOU2/src/Image.java
@@ -1,8 +1,62 @@
+import java.util.ArrayList;
+
 /**
  * Class that represent the image.
  * @author oi15ign, oi15sfn
  *
  */
-public class Image {
+public class Image
+{
+    private String imageName;
+    private ArrayList<Double> imageArray = new ArrayList();
+    private int caseClass;
 
+    /**
+     *
+     * @param imageName
+     * @param imageArray
+     * @param caseClass
+     */
+    public Image(String imageName, ArrayList imageArray, int caseClass)
+    {
+        this.imageName = imageName;
+        this.imageArray = imageArray;
+        this.caseClass = caseClass;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getImageName()
+    {
+        return imageName;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public ArrayList<Double> getImageArray()
+    {
+        return imageArray;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getCaseClass()
+    {
+        return caseClass;
+    }
+
+    /**
+     *
+     * @param caseClass
+     */
+    public void setCaseClass(int caseClass)
+    {
+        this.caseClass = caseClass;
+    }
 }
