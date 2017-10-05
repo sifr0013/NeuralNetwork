d61ee5b82a1bcbd335b95df1c432ffa55fed33ad
Simon Fredriksson <oi15sfn@cs.umu.se>
2017-10-05 13:38:19 +0200

    Made some changes to the Network and the Neuron
    Made so the Neuron takes a single image instead of a whole ImageReader.

--- AIOU2/src/NeuralNetwork.java
+++ AIOU2/src/NeuralNetwork.java
@@ -3,9 +3,20 @@
  */
 public class NeuralNetwork
 {
+    private Neuron Happy;
+    private Neuron Sad;
+    private Neuron Mischievous;
+    private Neuron Mad;
 
-    public NeuralNetwork()
+    ImageReader iR;
+
+    public NeuralNetwork(ImageReader imageReader, double learningRate, int arraySize)
     {
+        iR = imageReader;
+        Happy = new Neuron(imageReader,1,learningRate, arraySize);
+        Sad = new Neuron(imageReader,2,learningRate, arraySize);
+        Mischievous = new Neuron(imageReader,3,learningRate, arraySize);
+        Mad = new Neuron(imageReader,4,learningRate, arraySize);
 
     }
 }
--- AIOU2/src/Neuron.java
+++ AIOU2/src/Neuron.java
@@ -1,8 +1,78 @@
+import java.util.ArrayList;
+
+import static java.lang.Math.exp;
+
 /**
  * Class for creating a neuron.
  * @author oi15ign, oi15sfn
  *
  */
-public class Neuron {
+public class Neuron
+{
+    private Image img;
+    private int expectedFaceType;
+    private ArrayList<Double> weightList;
+    private double learningRate;
+    private double error;
+    private int trainingIteration = 0;
+    private double biasWeight = 1.0;
+
+
+    public Neuron(Image image, int expectedFaceType, double learningRate, int arraySize)
+    {
+        img = image;
+        this.expectedFaceType = expectedFaceType;
+        this.learningRate = learningRate;
+        weightList = new ArrayList<>(arraySize);
+
+        initializeWeightList();
+
+    }
+
+    private void initializeWeightList()
+    {
+        for (int i = 0; i < weightList.size(); i++)
+        {
+            weightList.add(i,Math.random());
+        }
+    }
+
+    /**
+     *
+     * @return 0, if the Neuron does not recognize the face.
+     * @return 1, if the Neuron does recognize the face.
+     */
+    public int guess()
+    {
+        for (int i = 0; i < weightList.size(); i++)
+        {
+
+        }
+
+    }
+
+    public void train()
+    {
+
+    }
+
+    public void test()
+    {
+
+    }
+
+
+
+    /**
+     * Private activation function. Sigmoid.
+     * @param x
+     * @return
+     */
+    private double activationFunction(double x)
+    {
+        return (1/(1+exp(-x)));
+    }
+
+
 
 }
